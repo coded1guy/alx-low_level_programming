0d90d7a41a17b6b85d691ab9a153d6f2eeb17e6c
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2022-09-16 00:44:25 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,28 @@
 
 void print_square(int size)
 {
-	int row, column;
 
-	for (row = 1; row <= size; ++row)
-	{
-		for (column = 1; column <= size; ++column)
-			_putchar('#');
-		_putchar('\n');
-	}
+int i;
+int k;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+
+for (k = 0; k < size; k++)
+{
+_putchar (35);
+}
+_putchar ('\n');
+
+}
+
+}
+
+else
+{
+_putchar ('\n');
+}
+
 }
